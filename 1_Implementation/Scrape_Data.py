cb2f3edabf7601776b6f59315e94f470871df5b2
Sagar Paryani <paryani7sagar@gmail.com>
2022-09-13 22:56:55 +0530

    update finished analysis

---
+++ 1_Implementation/Scrape_Data.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+import pandas as pd
+from bs4 import BeautifulSoup
+
+url = "https://prosettings.net/valorant-pro-settings-gear-list/"
+
+
+driver = webdriver.Chrome(executable_path="3_Dependencies/chromedriver.exe")
+
+driver.maximize_window()
+driver.get(url)
+driver.execute_script("window.scrollTo(0,10000)")
+time.sleep(5)
+
+page = driver.page_source
+soup = BeautifulSoup(page,"html.parser")
+season_stats = soup.find(id="table_1")
+
+df = pd.read_html(str(season_stats))[0]
+df.to_csv("2_Dataset\Pro_Settings.csv")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Perfect Sensitivity for FPS Gaming
  
+https://public.tableau.com/app/profile/sagar.paryani/viz/Perfectvalorantsettings/Story1?publish=yes
